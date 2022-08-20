37fa5320dcabf26d817ff14d8cce545ae6d64f2f
Molibden111 <89937059+Molibden111@users.noreply.github.com>
2022-08-20 20:28:16 +0400

    6
    сделала меню и футер почти

--- source/js/app.js
+++ source/js/app.js
@@ -2,10 +2,22 @@ var popup = document.querySelector('.popup');
 var popupBtn = document.querySelector('.banner .button');
 var popupClose = document.querySelector('.popupClose');
 
+var mobileBurger = document.querySelector('.humburger');
+var mobileClose = document.querySelector('.mobile-header__close');
+var mobileHeader = document.querySelector('.mobile-header');
+
 popupBtn.addEventListener('click', function(){
     popup.classList.add('popupOpen');
 })
 
 popupClose.addEventListener('click', function(){
     popup.classList.remove('popupOpen');
+})
+
+mobileBurger.addEventListener('click', function(){
+    mobileHeader.classList.add('mobile-header__open');
+})
+
+popupClose.addEventListener('click', function(){
+    mobileHeader.classList.remove('mobile-header__open');
 })
